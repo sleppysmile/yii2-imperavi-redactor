c4632391ebaffe1570e295219a12b01f376fafd3
i.podgornyy <i.podgorny@digital-sector.ru>
2019-11-20 15:30:22 +0300

    Alt and caption

--- assets/redactor.js
+++ assets/redactor.js
@@ -7750,7 +7750,7 @@
                         var node2 = this.selection.getMarker(2);
                         this.selection.setMarker(this.range, node2, false);
 
-                        if (this.utils.browser('chrome'))
+                        if (this.utils.browser('webkit'))
                         {
                             this.caret.set(node1, 0, node2, 0);
                         }
